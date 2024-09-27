fd1251b55e2300de3cd06626dbc8494698a6eb09
Sr1jan27 <singhsrijan809@gmail.com>
2024-09-27 20:29:50 +0530

    27

---
+++ faltu2.java
@@ -0,0 +1,9 @@
+public class faltu2 {
+    class Solution{
+        static void printIntString(String S,int N){
+            // code here
+            System.out.println("The input string :"+ S);
+            System.out.println("The input integer :"+ N);
+        }
+    }
+}
