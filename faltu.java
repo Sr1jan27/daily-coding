b994705c6cc08270d5ed085729c4dc33d00b6c70
Sr1jan27 <singhsrijan809@gmail.com>
2024-09-27 20:28:22 +0530

    27

---
+++ faltu.java
@@ -0,0 +1,12 @@
+public class faltu {
+    class Sol
+{
+   String conCat(String s1 , String s2)
+   {
+       //code here.
+       StringBuilder sb = new StringBuilder(s1);
+       sb.append(s2);
+       return sb.toString();
+   }
+}
+}
