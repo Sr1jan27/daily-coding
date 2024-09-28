398ebeece356241495cf1561b204e84abfe71987
Sr1jan27 <singhsrijan809@gmail.com>
2024-09-28 08:58:21 +0530

    28

---
+++ leastcost.java
@@ -0,0 +1,22 @@
+public class leastcost {
+    
+class Solution {
+    public int minimizeCost(int k, int arr[]) {
+        // code here
+         int[]dp = new int[arr.length];
+        for(int i = arr.length-2 ; i >= 0 ;i--){
+            int mini = Integer.MAX_VALUE;
+            for(int j = 1 ; j <= k ;j++){
+                if(i+j < arr.length){
+                    int x = dp[i+j];
+                        x += Math.abs(arr[i] - arr[i+j]);
+                        mini =  Math.min(mini,x);
+                }
+            }
+            dp[i] = mini;
+        }
+        return dp[0];
+    }
+}
+
+}
