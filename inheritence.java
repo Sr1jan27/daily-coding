109b26cfc69f59d917a94e0c8621702edf62e10d
Sr1jan27 <singhsrijan809@gmail.com>
2024-10-04 13:58:37 +0530

    04

---
+++ inheritence.java
@@ -0,0 +1,15 @@
+public class inheritence {
+        class Solution
+    {
+        public long sum_of_ap(long n, long a, long d)
+        {
+            // Code her
+            long s  =0 ;
+            for(int i=1;i<=n;i++){
+                s = s + a+(i-1)*d;
+            }
+            return s;
+        }
+    }
+        
+}
