e8b0b3d1885b98bfca06e8570ed2555291157077
Sr1jan27 <80779925+Sr1jan27@users.noreply.github.com>
2024-10-08 20:01:36 +0530

    08

---
+++ geeks08.java
@@ -0,0 +1,12 @@
+  int a = Integer.MIN_VALUE;
+        int b = Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            if(a<arr[i]){
+                b = a;
+                a = arr[i];
+            }
+            else if(b<arr[i]){
+                b = arr[i];
+            }
+        }
+        return a+b;
