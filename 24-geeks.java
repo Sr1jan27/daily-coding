26f028a4a693f398ea137430a3faf4dc610bcb20
Sr1jan27 <80779925+Sr1jan27@users.noreply.github.com>
2024-10-24 19:02:50 +0530

    Create 24-geeks.java

---
+++ 24-geeks.java
@@ -0,0 +1,28 @@
+class Solution {
+    static ArrayList<Integer> modifyAndRearrangeArr(int arr[]) {
+        // Complete the function
+         ArrayList<Integer> l1 = new ArrayList<Integer>();
+        
+        for(int i=0;i<arr.length-1;i++)
+        {
+            if(arr[i] == arr[i+1])
+            {
+                arr[i] = 2*arr[i];
+                arr[i+1] = 0;
+            }
+            if(arr[i] != 0)
+            {
+                l1.add(arr[i]);
+            }
+            
+        }
+        l1.add(arr[arr.length-1]);
+        int j=l1.size();
+        while(j< arr.length)
+        {
+            l1.add(0);
+            j++;
+        }
+        return l1;
+    }
+}
