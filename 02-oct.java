52bc691340c3780eb0ace09f897a737f7ac286cf
Srijan Singh <80779925+Sr1jan27@users.noreply.github.com>
2025-03-04 20:16:53 +0530

    Update 02-oct.java

--- 02-oct.java
+++ 02-oct.java
@@ -16,7 +16,6 @@ public class 02-oct {
             }
             return 0;
                 int start  = 0;
-
             HashMap<Character, Integer> hm = new HashMap<>();
             int max = 0;
             for(int end = 0;end<length;end++){
