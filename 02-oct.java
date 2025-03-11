c5a5622ec3fe3e6d52b84fe2539c79c535a2a41a
Srijan Singh <80779925+Sr1jan27@users.noreply.github.com>
2025-03-11 20:03:09 +0530

    Update 02-oct.java

--- 02-oct.java
+++ 02-oct.java
@@ -13,6 +13,8 @@ public class 02-oct {
             for(int i = 0; i < 4; i++){
                 if(a+i == n)return arr.get(b[i]+j-1);
 
+
+                
             }
             return 0;
                 int start  = 0;
